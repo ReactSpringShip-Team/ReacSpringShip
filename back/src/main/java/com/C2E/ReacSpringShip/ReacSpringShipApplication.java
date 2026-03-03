c3ad5f7dbd82b0351c944cdd800985baec9069cc
Einar115 <einarnaim@hotmail.com>
2026-03-02 19:41:03 -0600

    (chore) estructura inicial del backend

---
+++ back/src/main/java/com/C2E/ReacSpringShip/ReacSpringShipApplication.java
@@ -0,0 +1,13 @@
+package com.C2E.ReacSpringShip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReacSpringShipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReacSpringShipApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/C2E/ReacSpringShip/ReacSpringShipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.C2E.ReacSpringShip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReacSpringShipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
