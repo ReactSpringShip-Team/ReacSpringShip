28f6b0bffab83f78411f78bff84ae5df369e7cb3
CarlosGitUser <carlosalberto_c.martinez@hotmail.com>
2026-03-18 16:54:29 -0600

    refactor: creating input component for login and register form

---
+++ app/src/shared/components/Input.tsx
@@ -0,0 +1,36 @@
+import type { ColorType } from "../types/color.types";
+
+type InputType = 'text' | 'password' | 'email';
+
+interface Props {
+    type?: InputType;
+    value?: string;
+    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    placeHolder?: string;
+    color?: ColorType;
+}
+
+const inputColors: Record<ColorType, string> = {
+    pink: 'border-[#d946ef] shadow-[0_0_15px_#d946ef] focus:shadow-[0_0_25px_#d946ef]',
+    blue: 'border-cyan-400 shadow-[0_0_15px_#22d3ee] focus:shadow-[0_0_25px_#22d3ee]',
+    yellow: 'border-yellow-400 shadow-[0_0_15px_#facc15] focus:shadow-[0_0_25px_#facc15]',
+    purple: 'border-purple-500 shadow-[0_0_15px_#a855f7] focus:shadow-[0_0_25px_#a855f7]',
+};
+
+export const Input = ({ 
+    type = "text", 
+    value, 
+    onChange, 
+    placeHolder = "", 
+    color = 'pink' 
+}: Props) => {
+  return (
+    <input 
+        type={type} 
+        value={value}
+        onChange={onChange}
+        placeholder={placeHolder} 
+        className={`font-sans text-xl border-2 rounded-xl px-4 py-3 mb-6 outline-none w-full max-w-sm bg-transparent placeholder-[#94a3b8] text-white transition-shadow duration-300 ${inputColors[color]}`}
+    />
+  );
+}
---
+++ app/src/shared/types/color.types.ts
@@ -0,0 +1,1 @@
+export type ColorType =  'blue' | 'yellow' | 'pink' | 'purple';
