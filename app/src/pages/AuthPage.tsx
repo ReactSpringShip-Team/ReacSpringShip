7aca5676ba5f58c7825663bdc30a1fb33f323d2f
CarlosGitUser <carlosalberto_c.martinez@hotmail.com>
2026-03-18 16:58:11 -0600

    feature: adding navigation in AuthPage

--- app/src/pages/AuthPage.tsx
+++ app/src/pages/AuthPage.tsx
@@ -1,11 +1,28 @@
+import { useState } from "react";
 import { LoginForm } from "../features/auth/components/LoginForm"
+import { RegisterForm } from "../features/auth/components/RegisterForm";
 
+type formView = 'login' | 'register';
 export const AuthPage = () => {
-  return (
-    <div className="min-h-screen bg-[#051124] flex items-center justify-center">
-      <LoginForm>
 
-      </LoginForm>
+  const [view, setView] = useState<formView>('login');
+  return (
+    <div className="min-h-screen bg-[#051124] flex flex-col items-center justify-center">
+
+      <div className="flex gap-8 items-center text-white mb-6">
+        <button
+        className={`px-6 py-2 border-b-2 rounded-xl border-cyan-500 uppercase tracking-wider text-xl transition-all 
+        ${ view === 'login' ? 'border-cyan-400 text-cyan-400' : 'border-transparent text-slate-500 hover:text-slate-300'}`}
+        onClick={()=> setView('login')}>Login</button>
+        <button
+        className={`px-6 py-2 border-b-2 rounded-xl border-cyan-500 uppercase tracking-wider text-xl transition-all 
+        ${ view === 'register' ? 'border-cyan-400 text-cyan-400' : 'border-transparent text-slate-500 hover:text-slate-300'}`}
+        onClick={()=> setView('register')}>Register</button>
+      </div>
+
+      <div className="w-full flex justify-center">
+        { view === 'login' ? <LoginForm/> : <RegisterForm/>}
+      </div>
     </div>
   )
 }
