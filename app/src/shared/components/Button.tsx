8bbbd30749527536f08d36e7d0b46cbf17cd17ec
CarlosGitUser <carlosalberto_c.martinez@hotmail.com>
2026-03-18 16:55:02 -0600

    refactor: creating  Button component for register and login form

---
+++ app/src/shared/components/Button.tsx
@@ -0,0 +1,28 @@
+import type { ColorType } from "../types/color.types";
+
+
+interface Props{
+    text: string;
+    onClick?: ()=> void;
+    btnType?: 'submit' | 'button';
+    color?: ColorType;
+}
+
+const colors: Record<ColorType, string> = {
+    pink: 'border-[#db2777] text-[#db2777] shadow-[0_0_15px_#db2777] hover:bg-[#db2777]/20 hover:shadow-[0_0_25px_#db2777]',
+    blue: 'border-cyan-400 text-cyan-400 shadow-[0_0_15px_#22d3ee] hover:bg-cyan-400/20 hover:shadow-[0_0_25px_#22d3ee]',
+    yellow: 'border-yellow-400 text-yellow-400 shadow-[0_0_15px_#facc15] hover:bg-yellow-400/20 hover:shadow-[0_0_25px_#facc15]',
+    purple: 'border-purple-500 text-purple-500 shadow-[0_0_15px_#a855f7] hover:bg-purple-500/20 hover:shadow-[0_0_25px_#a855f7]',
+};
+
+export const Button = ({text, onClick, btnType = 'button', color = 'purple'} : Props) => {
+    return (
+        <button 
+                type={btnType}
+                onClick={onClick}
+                className={`font-sans text-xl border-2 rounded-xl px-4 py-3 transition-all duration-300 cursor-pointer uppercase tracking-wider font-black italic w-full max-w-sm ${colors[color]}`}
+            >
+                {text}
+        </button>
+        );
+}
