1dd65362d7a02c88a67e56bd4e3251fc5825e2a7
CarlosGitUser <carlosalberto_c.martinez@hotmail.com>
2026-03-20 12:00:15 -0600

    feat(Leaderboard): adding a shared navigation button

--- app/src/pages/LeaderboardPage.tsx
+++ app/src/pages/LeaderboardPage.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { GlobalScore } from "../features/leaderboard/components/GlobalScore";
 import { UserScore } from "../features/leaderboard/components/UserScore";
+import { BackButton } from "../shared/components/BackButton";
 
 type tab = 'global' | 'user';
 // Entender min-h-screen, backdrop-blur-md, w-full, max-w-2xl, drop-shadow, transition-all, 
@@ -9,7 +10,10 @@ export const LeaderboardPage = () => {
   const [activeTab, setActiveTab] = useState<tab>('global');
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-[#051124] font-sans text-white select-none">
+    <div className="relative flex flex-col items-center justify-center min-h-screen bg-[#051124] font-sans text-white select-none">
+      
+      <BackButton/>
+
       {/* Main container with Neon effect*/}
       <div className= "w-full max-w-2xl p-8 rounded-2xl border-2 border-cyan-500/50 bg-blue-900/20 backdrop-blur-md shadow-[0_0_20px_#22d3ee] font-sans">
         <h1 className="text-5xl font-bold text-center text-white italic tracking-tighter uppercase drop-shadow-[0_0_10px_#fff] mb-8">
