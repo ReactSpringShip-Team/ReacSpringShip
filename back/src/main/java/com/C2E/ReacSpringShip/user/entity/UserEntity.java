e42b83f59862a753f368483fd4279784971dcd79
Einar115 <einarnaim@hotmail.com>
2026-03-26 20:16:41 -0600

    feat: User entity model and repository created

---
+++ back/src/main/java/com/C2E/ReacSpringShip/user/entity/UserEntity.java
@@ -0,0 +1,91 @@
+package com.C2E.ReacSpringShip.user.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    public UUID id;
+
+    @Column(unique = true, nullable = false)
+    public String username;
+
+    @Column(name = "password_hash", nullable = false)
+    public String password;
+
+    @Column(unique = true, nullable = false)
+    public String email;
+
+    @Column(name = "created_at")
+    public LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(name = "enabled")
+    public Boolean enabled = true; //sujeto a cambios para la verificacion de email
+
+    public UserEntity(UUID id, String username, String password, String email, LocalDateTime createdAt) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.createdAt = createdAt;
+    }
+
+    public UserEntity(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public UserEntity() {
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+}
