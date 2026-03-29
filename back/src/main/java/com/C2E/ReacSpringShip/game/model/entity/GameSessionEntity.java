dab2459ca86a617ca48f549f3ca911bc7bebc3c5
Einar115 <einarnaim@hotmail.com>
2026-03-28 22:13:24 -0600

    feat: GameSession and PlayerResult entities were added in feature game

---
+++ back/src/main/java/com/C2E/ReacSpringShip/game/model/entity/GameSessionEntity.java
@@ -0,0 +1,72 @@
+package com.C2E.ReacSpringShip.game.model.entity;
+
+import com.C2E.ReacSpringShip.room.model.entity.RoomEntity;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "game_session")
+public class GameSessionEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id")
+    private RoomEntity room;
+
+    /*@Column(name = "result_data")
+    private Object resultData; */ // Not defined yet
+
+    @Column(name = "start_at")
+    private LocalDateTime startAt;
+
+    @Column(name = "end_at")
+    private LocalDateTime endAt;
+
+    @OneToMany(mappedBy = "session", fetch = FetchType.LAZY)
+    private List<PlayerResultEntity> playerResults;
+
+    public GameSessionEntity(RoomEntity room, LocalDateTime startAt, LocalDateTime endAt) {
+        this.room = room;
+        this.startAt = startAt;
+        this.endAt = endAt;
+    }
+
+    public GameSessionEntity() {}
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public RoomEntity getRoom() {
+        return room;
+    }
+
+    public void setRoom(RoomEntity room) {
+        this.room = room;
+    }
+
+    public LocalDateTime getStartAt() {
+        return startAt;
+    }
+
+    public void setStartAt(LocalDateTime startAt) {
+        this.startAt = startAt;
+    }
+
+    public LocalDateTime getEndAt() {
+        return endAt;
+    }
+
+    public void setEndAt(LocalDateTime endAt) {
+        this.endAt = endAt;
+    }
+}
---
+++ back/src/main/java/com/C2E/ReacSpringShip/game/model/entity/PlayerResultEntity.java
@@ -0,0 +1,61 @@
+package com.C2E.ReacSpringShip.game.model.entity;
+
+import com.C2E.ReacSpringShip.room.model.entity.RoomUserEntity;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "player_results")
+public class PlayerResultEntity {
+
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "session_id")
+    private GameSessionEntity session;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "participant_id")
+    private RoomUserEntity user;
+
+    private Long score;
+
+    private int placement;
+
+    public PlayerResultEntity(GameSessionEntity session, RoomUserEntity user, Long score, int placement) {
+        this.session = session;
+        this.user = user;
+        this.score = score;
+        this.placement = placement;
+    }
+
+    public GameSessionEntity getSession() {
+        return session;
+    }
+
+    public void setSession(GameSessionEntity session) {
+        this.session = session;
+    }
+
+    public RoomUserEntity getUser() {
+        return user;
+    }
+
+    public void setUser(RoomUserEntity user) {
+        this.user = user;
+    }
+
+    public Long getScore() {
+        return score;
+    }
+
+    public void setScore(Long score) {
+        this.score = score;
+    }
+
+    public int getPlacement() {
+        return placement;
+    }
+
+    public void setPlacement(int placement) {
+        this.placement = placement;
+    }
+}
