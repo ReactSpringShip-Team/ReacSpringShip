ae7699b99d64277057c973b8da57b1b6eb4259f1
Einar115 <einarnaim@hotmail.com>
2026-03-28 22:04:06 -0600

    feat: Swagger and docs endpoints were enabled in Spring Security

--- back/src/main/java/com/C2E/ReacSpringShip/config/security/SecurityConfig.java
+++ back/src/main/java/com/C2E/ReacSpringShip/config/security/SecurityConfig.java
@@ -27,7 +27,12 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(AbstractHttpConfigurer::disable) //Deshabilitado durante el desarrollo
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers(
+                                "/api/auth/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/v3/api-docs/**"
+                                ).permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session ->
