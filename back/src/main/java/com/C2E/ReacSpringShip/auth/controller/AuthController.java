8dc3ba384c3bcc28a886a9934f3498e3e14c49e3
Einar115 <einarnaim@hotmail.com>
2026-04-06 16:20:43 -0600

    refactor: API versioning was added to the URL path in the AuthController endpoints

--- back/src/main/java/com/C2E/ReacSpringShip/auth/controller/AuthController.java
+++ back/src/main/java/com/C2E/ReacSpringShip/auth/controller/AuthController.java
@@ -13,7 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/api/v1/auth")
 @Tag(name = "Auth", description = "for registration and authentication")
 public class AuthController {
 
