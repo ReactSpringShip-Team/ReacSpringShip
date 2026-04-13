784ead4b388b7ef4c480770ab5890ff5c8b1aa85
CarlosGitUser <carlosalberto_c.martinez@hotmail.com>
2026-04-13 15:47:46 -0600

    feat: implement SinglePlayerGame component and integrate it into SinglePlayerPage

---
+++ app/src/features/single-player/components/SinglePlayerGame.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { createPortal } from "react-dom";
+import { GameOverModal, HUD, PauseMenuModal } from "../../../shared";
+
+export const SinglePlayerGame = () => {
+  const [lives, seLives] = useState<number>(3);
+  const [time, setTime] = useState<number>(0);
+  const [score, setScore] = useState<number>(0);
+  const [showPauseMenu, setShowPauseMenu] = useState(false);
+  const [showGameOver, setGameOver] = useState(false);
+
+  const userName = 'user';
+
+  return (
+    <div className="min-h-screen bg-[#051124] flex flex-col text-white pt-4">
+      <HUD lives={lives} time={time} score={score} username={userName} />
+
+      <div id="canvas" className="flex-1 bg-gray-500 w-full "></div>
+
+      {/* === Modals ===*/}
+      {
+        showPauseMenu && createPortal(
+          <PauseMenuModal onContinue={() => setShowPauseMenu(false)} onExit={() => setShowPauseMenu(false)} />,
+          document.body
+        )
+      }
+
+      {
+        showGameOver && createPortal(
+          <GameOverModal onExit={() => setGameOver(false)} />, 
+          document.body
+        )
+      }
+    </div>
+  );
+};
