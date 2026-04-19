ace10f2c4b6eb043dc23e46ca3084bbad6073c34
Einar115 <einarnaim@hotmail.com>
2026-04-19 13:53:48 -0600

    feat: Validations were added to the DTO login request.

--- back/src/main/java/com/C2E/ReacSpringShip/auth/dto/request/LoginRequest.java
+++ back/src/main/java/com/C2E/ReacSpringShip/auth/dto/request/LoginRequest.java
@@ -1,12 +1,16 @@
 package com.C2E.ReacSpringShip.auth.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 
 @Schema(description = "Login request")
 public record LoginRequest(
         @Schema(description = "username of registered user", example = "Einar115")
+        @NotBlank(message = "The username is required.")
         String username,
-        @Schema(description = "password of registered user", example = "ReacSpringShip")
+
+        @Schema(description = "password of registered user", example = "ReacSpringShip-123")
+        @NotBlank(message = "A password is required.")
         String password
 ) {
 }
