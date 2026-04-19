c13cc1ea8a7daceae6f7022cd399b8851db6986f
Einar115 <einarnaim@hotmail.com>
2026-04-18 22:30:59 -0600

    feat: Custom exceptions were created to handle and categorize errors.

--- back/src/main/java/com/C2E/ReacSpringShip/common/exception/DuplicateResourceException.java
+++ back/src/main/java/com/C2E/ReacSpringShip/common/exception/DuplicateResourceException.java
@@ -1,4 +1,7 @@
 package com.C2E.ReacSpringShip.common.exception;
 
-public class DuplicateResourceException {
+public class DuplicateResourceException extends RuntimeException {
+    public DuplicateResourceException(String message) {
+        super(message);
+    }
 }
--- back/src/main/java/com/C2E/ReacSpringShip/common/exception/ResourceNotFoundException.java
+++ back/src/main/java/com/C2E/ReacSpringShip/common/exception/ResourceNotFoundException.java
@@ -1,4 +1,7 @@
 package com.C2E.ReacSpringShip.common.exception;
 
-public class ResourceNotFoundException {
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String resource, Object id) {
+        super(resource + " con id '" + id + "' no encontrado");
+    }
 }
